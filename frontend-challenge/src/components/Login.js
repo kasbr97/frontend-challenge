dab77f60fff27780d27a430b331e586ca540ff4c
kasbr97 <kev.asbr@gmail.com>
2024-07-20 00:40:22 -0700

    Added Poppins font, starting to add css for login page

---
+++ frontend-challenge/src/components/Login.js
@@ -0,0 +1,36 @@
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="background-image">
+            <div className="centered-div">
+                <div className="left-side-div">
+                    <p className="font-title-lg">Learn to code by watching others</p>
+                    <p className='font-text-sm'>
+                        See how experienced developers solve problems
+                        in real-time. Watching scripted tutorials is great, 
+                        but understanding how developers think is unvaluable.
+                    </p>
+                </div>
+                <div className="right-side-div">
+                    <div className="announcement">
+                        <p className="announcement-text">
+                            Try it free 7 days <span className="regular-font">then $20/mo. thereafter</span>
+                        </p>
+                    </div>
+                    <div className="announcement-shadow"/>
+                    
+
+                    <div className="login-form">
+                        
+                    </div>
+                    
+
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Login;
